0b8a7d201f2ce2fff2c02ced68a0922bc316a867
idelen <ch.jackpot@gmail.com>
2023-07-02 20:02:22 +0900

    [03.TemplateMethod] base example

---
+++ src/TemplateMethod/TemplateMethodMain.java
@@ -0,0 +1,11 @@
+package TemplateMethod;
+
+public class TemplateMethodMain {
+    public static void main(String[] args) {
+        AbstractDisplay d1 = new CharDisplay('H');
+        AbstractDisplay d2 = new StringDisplay("Hello, world.");
+
+        d1.display();
+        d2.display();
+    }
+}
